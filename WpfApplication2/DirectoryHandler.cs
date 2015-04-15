4e0d29d2e60c0ce83c0f4981ea257704c72fd2cb
std66 <sinkutamas@gmail.com>
2015-04-15 23:01:49 +0200

    Initial import

---
+++ WpfApplication2/DirectoryHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    class DirectoryHandler
+    {
+        private string rootPath;
+
+        public event EventHandler<List<FileSystemInfo>> PathChanged;
+
+        public string RootPath
+        {
+            get
+            {
+                return rootPath;
+            }
+        }
+
+        public bool IsRoot
+        {
+            get
+            {
+                return this.rootPath.Split('\\').Length == 1;
+            }
+        }
+
+        public DirectoryHandler(string RootPath)
+        {
+            if (!Directory.Exists(RootPath))
+                throw new IOException("Ilyen könyvtár nem létezik");
+
+            this.rootPath = RootPath;
+
+            this.GetContents();
+        }
+
+        public void Update()
+        {
+            this.GetContents();
+        }
+
+        private void GetContents()
+        {
+            List<FileSystemInfo> Contents = new List<FileSystemInfo>();
+
+            DirectoryInfo Info = new DirectoryInfo(this.rootPath);
+
+            Contents.AddRange(Info.GetDirectories().OrderBy((x) => x.Name));
+            Contents.AddRange(Info.GetFiles().OrderBy((x) => x.Name));
+
+            if (this.PathChanged != null)
+                this.PathChanged(this, Contents);
+        }
+    }
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DirectoryHandler LeftDirectoryHandler;
+        private readonly DirectoryHandler RightDirectoryHandler;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var CurrentDrive in DriveInfo.GetDrives())
+            {
+                if (CurrentDrive.IsReady)
+                {
+                    cbDriveLeft.Items.Add(String.Format("{0} [{1}]", CurrentDrive.Name, CurrentDrive.VolumeLabel));
+                    cbDriveRight.Items.Add(String.Format("{0} [{1}]", CurrentDrive.Name, CurrentDrive.VolumeLabel));
+                }
+            }
+
+            if (cbDriveLeft.Items.Count == 0)
+            {
+                MessageBox.Show("Nincs meghajtó a rendszerben.");
+                Environment.Exit(1);
+            }
+
+            cbDriveLeft.SelectedIndex = 0;
+            cbDriveRight.SelectedIndex = 0;
+
+            this.LeftDirectoryHandler = new DirectoryHandler(DriveInfo.GetDrives()[0].RootDirectory.FullName);
+            this.RightDirectoryHandler = new DirectoryHandler(DriveInfo.GetDrives()[0].RootDirectory.FullName);
+
+            this.LeftDirectoryHandler.PathChanged += (o, e) => this.UpdateWindow(e, lwLeftWindow);
+            this.RightDirectoryHandler.PathChanged += (o, e) => this.UpdateWindow(e, lwRightWindow);
+
+            this.LeftDirectoryHandler.Update();
+            this.RightDirectoryHandler.Update();
+        }
+
+        private void UpdateWindow(List<FileSystemInfo> Contents, ListView Target)
+        {
+            Target.Items.Clear();
+
+            foreach (var Current in Contents)
+            {
+                if (Current is DirectoryInfo)
+                {
+                    var x = new
+                    {
+                        Name = Current.Name,
+                        Ext = "",
+                        Size = "<DIR>",
+                        LastAccess = Current.LastAccessTime.ToString()
+                    };
+
+                    Target.Items.Add(x);
+                }
+            }
+        }
+    }
+}
