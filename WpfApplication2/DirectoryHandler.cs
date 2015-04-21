109fff0dcc8188f8a11affc74981d13aa062fda3
std66 <sinkutamas@gmail.com>
2015-04-21 15:33:07 +0200

    Némi bindinges kísérlet...

--- WpfApplication2/DirectoryHandler.cs
+++ WpfApplication2/DirectoryHandler.cs
@@ -4,57 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
-namespace WpfApplication2
-{
-    class DirectoryHandler
-    {
-        private string rootPath;
+namespace WpfApplication2 {
+	class DirectoryHandler {
+		private string rootPath;
 
-        public event EventHandler<List<FileSystemInfo>> PathChanged;
+		public readonly ObservableCollection<FileSystemInfo> Contents = new ObservableCollection<FileSystemInfo>();
 
-        public string RootPath
-        {
-            get
-            {
-                return rootPath;
-            }
-        }
+		public string RootPath {
+			get {
+				return rootPath;
+			}
+		}
 
-        public bool IsRoot
-        {
-            get
-            {
-                return this.rootPath.Split('\\').Length == 1;
-            }
-        }
+		public bool IsRoot {
+			get {
+				return this.rootPath.Split('\\').Length == 1;
+			}
+		}
 
-        public DirectoryHandler(string RootPath)
-        {
-            if (!Directory.Exists(RootPath))
-                throw new IOException("Ilyen könyvtár nem létezik");
+		public DirectoryHandler(string RootPath) {
+			if (!Directory.Exists(RootPath))
+				throw new IOException("Ilyen könyvtár nem létezik");
 
-            this.rootPath = RootPath;
+			this.rootPath = RootPath;
 
-            this.GetContents();
-        }
+			this.GetContents();
+		}
 
-        public void Update()
-        {
-            this.GetContents();
-        }
+		public void Update() {
+			this.GetContents();
+		}
 
-        private void GetContents()
-        {
-            List<FileSystemInfo> Contents = new List<FileSystemInfo>();
+		private void GetContents() {
+			this.Contents.Clear();
 
-            DirectoryInfo Info = new DirectoryInfo(this.rootPath);
+			DirectoryInfo Info = new DirectoryInfo(this.rootPath);
 
-            Contents.AddRange(Info.GetDirectories().OrderBy((x) => x.Name));
-            Contents.AddRange(Info.GetFiles().OrderBy((x) => x.Name));
+			foreach (var item in Info.GetDirectories().OrderBy((x) => x.Name)) {
+				this.Contents.Add(item);
+			}
 
-            if (this.PathChanged != null)
-                this.PathChanged(this, Contents);
-        }
-    }
+			foreach (var item in Info.GetFiles().OrderBy((x) => x.Name)) {
+				this.Contents.Add(item);
+			}
+		}
+	}
 }
